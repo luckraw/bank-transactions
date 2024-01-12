aaedea5a69e67ddecf4a7dd005e2c779a3de7979
nulld <krawdev@gmail.com>
2024-01-12 20:12:43 -0300

    Adicionar arquivos de projeto.

---
+++ bank-transactions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bank_transactions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
